49a1fdd2c475dd303093d096fddb88bf263e7ef1
Lucas Pradella <lucas.pradella@hotmail.com>
2021-07-28 21:53:34 -0300

    Add anotations + create partitions.

--- src/main/java/br/com/pradella/ecommerce/NewOrderMain.java
+++ src/main/java/br/com/pradella/ecommerce/NewOrderMain.java
@@ -7,21 +7,25 @@ import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.common.serialization.StringSerializer;
 
 import java.util.Properties;
+import java.util.UUID;
 import java.util.concurrent.ExecutionException;
 
 public class NewOrderMain {
 
     public static void main(String[] args) throws ExecutionException, InterruptedException {
         var producer = new KafkaProducer<String, String>(properties());
-        var value = "Vai, Corinthians";
-        var record = new ProducerRecord<>("ECOMMERCE_NEW_ORDER", value, value);
-        producer.send(record, getCallback()).get();
+
+        for (int i= 0; i <100; i++){
+            var key = UUID.randomUUID().toString();
+            var value = key + ",Vai, Corinthians";
+            var record = new ProducerRecord<>("ECOMMERCE_NEW_ORDER", key, value);
+            producer.send(record, getCallback()).get();
 
 
-        var email = "Thank you for your order! We are processing your order!";
-        var emailRecord = new ProducerRecord<>("ECOMMERCE_SEND_EMAIL", email, email);
-        producer.send(emailRecord, getCallback()).get();
-
+            var email = "Thank you for your order! We are processing your order!";
+            var emailRecord = new ProducerRecord<>("ECOMMERCE_SEND_EMAIL", email, email);
+            producer.send(emailRecord, getCallback()).get();
+        }
     }
 
     private static Properties properties() {
