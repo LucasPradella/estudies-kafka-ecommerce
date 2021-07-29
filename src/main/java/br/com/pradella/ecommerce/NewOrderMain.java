ef968deeb73e43faeed96a79d09d37884dbd7376
Lucas Pradella <lucas.pradella@hotmail.com>
2021-07-29 11:25:43 -0300

    Add camada para abstrair configurações do Kafka

--- src/main/java/br/com/pradella/ecommerce/NewOrderMain.java
+++ src/main/java/br/com/pradella/ecommerce/NewOrderMain.java
@@ -13,36 +13,16 @@ import java.util.concurrent.ExecutionException;
 public class NewOrderMain {
 
     public static void main(String[] args) throws ExecutionException, InterruptedException {
-        var producer = new KafkaProducer<String, String>(properties());
+        try (var dispatcher = new KafkaDispatcher()) {
+            for (var i = 0; i < 10; i++) {
 
-        for (int i= 0; i <100; i++){
-            var key = UUID.randomUUID().toString();
-            var value = key + ",Vai, Corinthians";
-            var record = new ProducerRecord<>("ECOMMERCE_NEW_ORDER", key, value);
-            producer.send(record, getCallback()).get();
+                var key = UUID.randomUUID().toString();
+                var value = key + ",Vai,Corinthians";
+                dispatcher.send("ECOMMERCE_NEW_ORDER", key, value);
 
-
-            var email = "Thank you for your order! We are processing your order!";
-            var emailRecord = new ProducerRecord<>("ECOMMERCE_SEND_EMAIL", email, email);
-            producer.send(emailRecord, getCallback()).get();
-        }
-    }
-
-    private static Properties properties() {
-        var properties = new Properties();
-        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
-        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
-        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
-        return properties;
-    }
-
-    private static Callback getCallback() {
-        return (data, ex) -> {
-            if (ex != null) {
-                ex.printStackTrace();
-                return;
+                var email = "Thank you for your order! We are processing your order!";
+                dispatcher.send("ECOMMERCE_SEND_EMAIL", key, email);
             }
-            System.out.println("sucesso enviando " + data.topic() + ":::partition " + data.partition() + "/ offset " + data.offset() + "/ timestamp " + data.timestamp());
-        };
+        }
     }
 }
