1f3513eb380197b3d2d020feae69aba8be74a60a
Lucas Pradella <lucas.pradella@hotmail.com>
2021-07-30 16:26:39 -0300

    Add configuração de Serializer e Derializer

--- src/main/java/br/com/pradella/ecommerce/FraudDetectorService.java
+++ src/main/java/br/com/pradella/ecommerce/FraudDetectorService.java
@@ -3,18 +3,21 @@ package br.com.pradella.ecommerce;
 import br.com.pradella.ecommerce.model.Order;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 
+import java.util.HashMap;
+
 public class FraudDetectorService {
 
         public static void main(String[] args) {
             var fraudService = new FraudDetectorService();
             try(var service = new KafkaService(FraudDetectorService.class.getSimpleName(),
                     "ECOMMERCE_NEW_ORDER",
-                    fraudService::parse)) {
+                    fraudService::parse,
+                    Order.class, new HashMap<>())) {
                 service.run();
             }
         }
 
-        private void parse(ConsumerRecord<String, String> record) {
+        private void parse(ConsumerRecord<String, Order> record) {
             System.out.println("------------------------------------------");
             System.out.println("Processing new order, checking for fraud");
             System.out.println(record.key());
